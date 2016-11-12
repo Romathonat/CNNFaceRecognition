6c5c9bbb5f97b000954cde5b3e7ca01846287443
Théo <t.baseilhac@gmail.com>
2016-11-12 01:53:38 +0100

    Changed metric for DBSCAN to account for overlapping and simpler calculation, changed output of cluster drawing functions

--- DBSCAN/draft_dbscan.py
+++ DBSCAN/draft_dbscan.py
@@ -48,18 +48,21 @@ def draw_detections(detections, infilename, outfilename):
     
     skimage.io.imsave(outfilename, image)
 
-def draw_grouped_detections(clusters, infilename, outfilename):
+def draw_grouped_detections(clusters, in_source_filename, in_detections_filename, out_clusters_on_source_filename, out_clusters_on_detections_filname):
 
-    image = skimage.io.imread(infilename, as_grey=True)
+    image = skimage.io.imread(in_source_filename, as_grey=True)
+    image2 = skimage.io.imread(in_detections_filename, as_grey=True)
 
     for cluster_num, detection in clusters.items():
         x = int(detection[0]+0.5*detection[2])
         y = int(detection[1]+0.5*detection[3])
         half_diag = int(0.5*math.sqrt(math.pow(detection[2], 2)+math.pow(detection[3],2)))
         rr, cc = skimage.draw.circle_perimeter(x, y, half_diag)
-        image[rr, cc] = 255
+        image[rr, cc] = 1
+        image2[rr, cc] = 255
 
-    skimage.io.imsave(outfilename, image)
+    skimage.io.imsave(out_clusters_on_source_filename, image)
+    skimage.io.imsave(out_clusters_on_detections_filname, image2)
 
 def group_detections(detections, min_score):
 
@@ -136,23 +139,49 @@ def find_neighbors(detections, center_detection, epsilon):
 
     epsilon_neighbors = []
     for detection in detections:
-        if distance(detection, center_detection) <= epsilon:# and distance(detection, center_detection) != 0:
+        dist = distance(detection, center_detection)
+        if dist <= epsilon:
             epsilon_neighbors.append(detection)
     return epsilon_neighbors
 
 def distance(detection1, detection2):
 
+    # Rules : distance < 36 : cannot be two detectable separated faces because detector can only detect 36*36 or bigger
+
+    d1_centroid_x = detection1.top_left_corner.x+0.5*detection1.width
+    d1_centroid_y = detection1.top_left_corner.y+0.5*detection1.height
+    d2_centroid_x = detection2.top_left_corner.x+0.5*detection2.width
+    d2_centroid_y = detection2.top_left_corner.y+0.5*detection2.height
+
     # Euclidian distance
-    distance = math.sqrt(math.pow((detection2.top_left_corner.x-detection1.top_left_corner.x),2)+math.pow((detection2.top_left_corner.y-detection1.top_left_corner.y),2)+math.pow((detection2.width-detection1.width),2)+math.pow((detection2.height-detection1.height),2))
+    distance = math.sqrt(math.pow(d2_centroid_x-d1_centroid_x,2)+math.pow(d2_centroid_y-d1_centroid_y,2)+math.pow((detection2.width-detection1.width),2)+math.pow((detection2.height-detection1.height),2))
+    '''
+    # Calculate overlap
+    max_left = max(detection1.top_left_corner.x, detection2.top_left_corner.x)
+    min_right = min(detection1.top_left_corner.x+detection1.width, detection2.top_left_corner.x+detection2.width)
+    max_top = max(detection1.top_left_corner.y, detection2.top_left_corner.y)
+    min_bottom = min(detection1.top_left_corner.y+detection1.height, detection2.top_left_corner.y+detection2.height)
+
+    if(max_left < min_right and max_top < min_bottom):
+        #overlapping
+        
+        #calculate overalapping ratio
+        overlap_area = (min_bottom-max_top)*(min_right-max_left)
+        d1_area = detection1.width*detection1.height
+        d2_area = detection2.width*detection2.height
+        overlapping_ratio = overlap_area/(d1_area+d2_area-overlap_area)
+        distance = 1-overlapping_ratio
+        print(overlapping_ratio)
+    else:
+        distance = -1 #represents infinite
+    #print(distance)
+    '''
     return distance
 
 ##################################### Tests
 
 detections = load_detections()
-DBSCAN(detections, 35, 2)
+DBSCAN(detections, 36, 2)
 grouped_detections = group_detections(detections, 0.99)
-for detection in detections:
-    #print(detection.top_left_corner.x, detection.top_left_corner.y, detection.width, detection.height, detection.clustering_status)
-    pass
 draw_detections(detections, 'merica.jpg', 'merica_with_detections.jpg')
-draw_grouped_detections(grouped_detections, 'merica_with_detections.jpg', 'merica_with_detections_and_clusters.jpg')
+draw_grouped_detections(grouped_detections, 'merica.jpg', 'merica_with_detections.jpg', 'merica_with_clusters_on_source.jpg', 'merica_with_clusters_on_detections.jpg')
