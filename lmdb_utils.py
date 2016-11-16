9ccba896b1d0529e44bfb3558f825998708afeeb
Romathonat <romain.mathonat@gmail.com>
2016-11-16 18:14:50 +0100

    adding generate_lmdb_from_non_faces

--- lmdb_utils.py
+++ lmdb_utils.py
@@ -1,5 +1,7 @@
 import lmdb
+import glob,os
 from random import randint
+from subprocess import call
 
 def merge_lmdb(path_lmdb1, path_lmdb2, out_path):
     txn1 = lmdb.open(path_lmdb1).begin(write=True)
@@ -31,4 +33,22 @@ def generate_posneg_faces(number_faces_to_generate):
     with open('posneg_iteration.txt', 'w+') as f:
         f.write(''.join(extracted_lines))
 
-generate_posneg_faces(10000)
+def generate_lmdb_from_non_faces(path_non_faces):
+    local_posneg = "textures_posneg.txt"
+    output = "textures_lmdb"
+
+    with open("./{}".format(local_posneg), 'w+') as f:
+        os.chdir("/.")
+        for file in glob.glob("*.pgm"):
+            f.write('{} 0\n'.format(file.name))
+
+    call(["convert_imageset", "--suffle", "--gray", path_non_faces, local_posneg, output])
+
+    #we can delete this file now
+    os.remove('./{}'.format(output))
+
+#1 we train the network with 20000 F
+#2 we make him work on textures images (so non-faces), and we isolate the ones that are > THR (detected as faces)
+#3 we generate lmdb file from those isolated images
+#4 we generate new F and NF for next iterations: merge NF and generate_posneg_faces for faces
+#5 goto 1
