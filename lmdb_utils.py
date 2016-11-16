1e4be27cf34b0d7d35d42bbf79363a876737db2b
Romathonat <romain.mathonat@gmail.com>
2016-11-16 17:45:34 +0100

    adding function that pick ramdom face among those in posneg.txt

--- lmdb_utils.py
+++ lmdb_utils.py
@@ -1,4 +1,5 @@
 import lmdb
+from random import randint
 
 def merge_lmdb(path_lmdb1, path_lmdb2, out_path):
     txn1 = lmdb.open(path_lmdb1).begin(write=True)
@@ -14,4 +15,20 @@ def merge_lmdb(path_lmdb1, path_lmdb2, out_path):
         for (key, value) in database2:
             txn3.put(key, value)
 
-merge_lmdb('./train_lmdb', './test_lmdb', './test')
+def generate_posneg_faces(number_faces_to_generate):
+    #we read from posneg.txt, then we randomly select among the faces
+    #64770: number of positive images
+    line_to_extract = [randint(0, 64770) for x in range(number_faces_to_generate)]
+
+    in_memory_posneg = []
+
+    with open('posneg.txt', 'r') as f:
+        for line in f:
+            in_memory_posneg.append(line)
+
+    extracted_lines = [in_memory_posneg[i] for i in line_to_extract]
+
+    with open('posneg_iteration.txt', 'w+') as f:
+        f.write(''.join(extracted_lines))
+
+generate_posneg_faces(10000)
